7c9c1e8948e3dfb12d3b238f9b2946018d8d3d2d
Kespriu <kitty.karen@live.com>
2025-12-06 19:11:54 -0600

    Begining Setup, making classes and setting imports

---
+++ src/citkat_code.py
@@ -0,0 +1,26 @@
+import tkinter
+import time
+import pygame
+
+# Getting an actual widget on screen and getting it to move around. (Make pixel art of CitKat Clock).
+# Getting the desktop toy to be pinned above other applications.
+# Implementing the text bubbles and having them pop up when the Kat is clicked on.
+# Making dialogue options for said text bubbles.
+# Getting the clock on the Kats Tummy to tell the time and show it in real time through animation.
+# Getting the clock to pop up when clicked on and final touch ups.
+
+class ClockImageImport(): 
+
+class ClockFaceAnimation():
+
+class TailAnimation():
+
+class DialougeChoices():
+
+class Pinning():
+
+def main():
+    
+
+if __name__ == "__main__":
+    main()
